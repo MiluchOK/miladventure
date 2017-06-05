f7a6de257c4e332ecb3cc93aad2d751e3f4422e5
Alexey Milyukov <milyukov.alexey@gmail.com>
2017-06-05 01:01:27 -0700

    Start with react-bootstrap

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -4,23 +4,19 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import Button from 'react-bootstrap/lib/Button';
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
+class Hello extends React.Component {
+    render() {
+        return(
+            <div>
+                Hello {this.props.name}!
+                <Button bsStyle="primary" bsSize="large">Large button</Button>
+            </div>
+        )
+    }
 }
 
-Hello.propTypes = {
-  name: PropTypes.string
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
+document.addEventListener("DOMContentLoaded", e => {
+    ReactDOM.render(<Hello name="Miladventure" />, document.body.appendChild(document.createElement('div')))
 })
