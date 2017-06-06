e30978573d383bffa2103938dd067b478ae1e4b2
alexey <amilyukov@amilyukov.local>
2017-06-05 21:36:27 -0700

    Adding main stuff

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -1,22 +1,14 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
+/**
+ * Created by amilyukov on 6/5/17.
+ */
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Button from 'react-bootstrap/lib/Button';
-
 class Hello extends React.Component {
     render() {
-        return(
-            <div>
-                Hello {this.props.name}!
-                <Button bsStyle="primary" bsSize="large">Large button</Button>
-            </div>
-        )
+        return <div>Hello {this.props.name}!</div>
     }
 }
-
 document.addEventListener("DOMContentLoaded", e => {
-    ReactDOM.render(<Hello name="Miladventure" />, document.body.appendChild(document.createElement('div')))
+    ReactDOM.render(<Hello name="React" />, document.body.appendChild(document.createElement('div')))
 })
